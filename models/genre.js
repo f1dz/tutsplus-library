72928537ddc6d63d68fb95a5accc154b276a325a
Khofidin <offiedz@gmail.com>
2019-02-15 15:17:20 +0700

    Worked

---
+++ models/genre.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose')
+mongoose.Promise = global.Promise
+const Schema = mongoose.Schema
+
+const genreSchema = Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a Genre name'
+  }
+})
+module.exports = mongoose.model('Genre', genreSchema)
